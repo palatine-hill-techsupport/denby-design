5a757a1b2a16c1645a71469f5a5a71a035298787
Den <38339489+palatine-hill-techsupport@users.noreply.github.com>
2025-03-19 12:41:16 +1100

    Update scripts.js
    Updated placeholder text and bio section.
    Created structure to parallax effect div. 
    Improved mobile responsiveness.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,7 @@
-//coming soon
+window.addEventListener("scroll", () => {
+    if (window.scrollY > window.innerHeight * 0.1) { // Triggers earlier
+        document.body.classList.add("scrolled");
+    } else {
+        document.body.classList.remove("scrolled");
+    }
+});
